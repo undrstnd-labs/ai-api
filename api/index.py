462b7fdc509043d5742a479e925a43ffcd904bc3
FindMalek <hi@findmalek.com>
2024-10-02 19:22:12 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ api/index.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+
+### Create FastAPI instance with custom docs and openapi url
+app = FastAPI(docs_url="/api/py/docs", openapi_url="/api/py/openapi.json")
+
+@app.get("/api/py/helloFastApi")
+def hello_fast_api():
+    return {"message": "Hello from FastAPI"}
