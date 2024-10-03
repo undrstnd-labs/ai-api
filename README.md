6449907868d3dece244edc0a80a326a10a1a06d3
findmalek <hi@findmalek.com>
2024-10-03 01:26:14 +0100

    chore(test): test with env

--- README.md
+++ README.md
@@ -55,21 +55,13 @@ source venv/bin/activate
 Then, install the dependencies:
 
 ```bash
-npm install
-# or
-yarn
-# or
-pnpm install
+pip3 install -r requirements.txt
 ```
 
 Then, run the development server(python dependencies will be installed automatically here):
 
 ```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
+sh run.sh
 ```
 
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
@@ -85,3 +77,11 @@ To learn more about Next.js, take a look at the following resources:
 - [FastAPI Documentation](https://fastapi.tiangolo.com/) - learn about FastAPI features and API.
 
 You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
+
+### Testing
+
+To run the tests, run the following command:
+
+```bash
+sh test.sh
+```
--- tests/test_compilation.py
+++ tests/test_compilation.py
@@ -5,24 +5,23 @@ from dotenv import load_dotenv
 def main():
     load_dotenv()
 
-    production_url = "https://ai-api-o4rf.onrender.com/v1"
+    production_url = "https://ai-api-u74q.onrender.com/v1"
     development_url = "http://localhost:3000/v1"
-    fastapi_url = "http://127.0.0.1:8000/v1"
 
-    base_url = production_url if os.environ.get("ENV") == "production" else fastapi_url
+    base_url = development_url if os.environ.get("ENV") == "production" else production_url
 
     is_streaming = True
 
     client = OpenAI(
         api_key="udsk_demo-api-key-x-00000",
-        base_url=production_url
+        base_url=base_url
     )
 
     chat_completion = client.chat.completions.create(
         messages=[
             {
                 "role": "user",
-                "content": "What is the meaning of life?",
+                "content": "Give me a summary of the meaning of life",
             }
         ],
         model="llama3-8b-8192",
