1bc5bbff392d7ab5b4c51ae3638f31d9969737c0
findmalek <hi@findmalek.com>
2024-10-03 01:32:33 +0100

    chore(test): test with env

--- tests/test_compilation.py
+++ tests/test_compilation.py
@@ -5,10 +5,10 @@ from dotenv import load_dotenv
 def main():
     load_dotenv()
 
-    production_url = "https://ai-api-u74q.onrender.com/v1"
     development_url = "http://localhost:3000/v1"
+    production_url = "https://ai-api-u74q.onrender.com/v1"
 
-    base_url = development_url if os.environ.get("ENV") == "production" else production_url
+    base_url = production_url if os.environ.get("ENV") == "production" else development_url
 
     is_streaming = True
 
